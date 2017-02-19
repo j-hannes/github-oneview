bc0f36ac17aacb78256ce875c96b53ee9b66bb88
j-hannes <hannes_e@gmx.de>
2017-02-19 12:25:22 +0100

    Use standard JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 var path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [new HtmlWebpackPlugin()]
 }
