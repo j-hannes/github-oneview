591f2732a61479250702ab3be8c9e8a5fe2d80ba
j-hannes <hannes_e@gmx.de>
2017-02-24 23:01:07 +0100

    Use condensed form for epic

--- src/modules/content-list/internal/epics/fetchContents.js
+++ src/modules/content-list/internal/epics/fetchContents.js
@@ -4,32 +4,18 @@ import * as selectRepo from 'modules/select-repo'
 
 export const fetchContents = (action$, store) =>
   action$.ofType(selectRepo.actionTypes.SUBMIT_FORM)
-    .map(() => selectRepoStore(store))
-    .map(prepareData)
-    .switchMap(makeRequest)
-
-const selectRepoStore = store => store.getState().get(selectRepo.name)
-
-const prepareData = state => ({
-  user: state.user,
-  repo: state.repo
-})
-
-const makeRequest = ({user, repo}) =>
-  ajax
-    .getJSON(endpoint(user, repo))
-    .map(extractData)
-    .map(receiveContents)
-
-export const endpoint = generateUrl('https://api.github.com')
-
-export const localEndPoint = generateUrl('http://localhost:3000')
-
-function generateUrl (host) {
-  return (user, repo) => `${host}/repos/${user}/${repo}/contents`
-}
-
-const extractData = contentList =>
-  contentList.map(contentItem => ({
-    name: contentItem.name
-  }))
+    .map(() => store.getState().get(selectRepo.name))
+    .map(state => ({
+      user: state.user,
+      repo: state.repo
+    }))
+    .switchMap(({user, repo}) =>
+      ajax
+        .getJSON(`https://api.github.com/repos/${user}/${repo}/contents`)
+        // .getJSON(`http://localhost:3000/repos/${user}/${repo}/contents`)
+        .map(contentList =>
+          contentList.map(contentItem => ({
+            name: contentItem.name
+          })))
+        .map(receiveContents)
+    )
