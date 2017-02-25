cb1a5b6aa4d1218c1c6543fb3c364c435f79820c
j-hannes <hannes_e@gmx.de>
2017-02-25 10:32:55 +0100

    Simplify epic

--- src/modules/content-list/internal/epics/fetchContents.js
+++ src/modules/content-list/internal/epics/fetchContents.js
@@ -1,21 +1,13 @@
 import { ajax } from 'rxjs/observable/dom/ajax'
-import { receiveContents } from './actions'
+import { receiveContents } from '../actions'
 import * as selectRepo from 'modules/select-repo'
+
+const serverCall = ({ user, repo }) =>
+  ajax.getJSON(`https://api.github.com/repos/${user}/${repo}/contents`)
 
 export const fetchContents = (action$, store) =>
   action$.ofType(selectRepo.actionTypes.SUBMIT_FORM)
     .map(() => store.getState().get(selectRepo.name))
-    .map(state => ({
-      user: state.user,
-      repo: state.repo
-    }))
-    .switchMap(({user, repo}) =>
-      ajax
-        .getJSON(`https://api.github.com/repos/${user}/${repo}/contents`)
-        // .getJSON(`http://localhost:3000/repos/${user}/${repo}/contents`)
-        .map(contentList =>
-          contentList.map(contentItem => ({
-            name: contentItem.name
-          })))
-        .map(receiveContents)
-    )
+    .flatMap(serverCall)
+    .map(contentList => contentList.map(({ name }) => ({ name })))
+    .map(receiveContents)
