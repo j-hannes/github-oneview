c79a010435aca895694123f63269de6c08fa6265
j-hannes <hannes_e@gmx.de>
2017-02-25 12:38:45 +0100

    Refactor select props

--- src/modules/content-list/internal/epics/fetchContents.js
+++ src/modules/content-list/internal/epics/fetchContents.js
@@ -1,13 +1,16 @@
 import { ajax } from 'rxjs/observable/dom/ajax'
 import { receiveContents } from '../actions'
 import * as selectRepo from 'modules/select-repo'
+import { compose, map, pick } from 'ramda'
 
 const serverCall = ({ user, repo }) =>
   ajax.getJSON(`https://api.github.com/repos/${user}/${repo}/contents`)
+
+const selectProps = compose(map, pick)
 
 export const fetchContents = (action$, store) =>
   action$.ofType(selectRepo.actionTypes.SUBMIT_FORM)
     .map(() => store.getState().get(selectRepo.name))
     .flatMap(serverCall)
-    .map(contentList => contentList.map(({ name, sha }) => ({ name, sha })))
+    .map(selectProps(['name', 'sha']))
     .map(receiveContents)
