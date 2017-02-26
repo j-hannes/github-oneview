821138a81c11254feeaa442d58972217e2a8e583
j-hannes <hannes_e@gmx.de>
2017-02-26 09:30:51 +0100

    Add headline to application

--- src/modules/app/internal/components/App.js
+++ src/modules/app/internal/components/App.js
@@ -3,10 +3,18 @@ import React from 'react'
 import { SelectRepo } from 'modules/select-repo'
 import { ContentList } from 'modules/content-list'
 
+const style = {
+  headline: {
+    margin: '10px',
+    marginBottom: '4px'
+  }
+}
+
 class App extends React.Component {
   render () {
     return (
       <div>
+        <h3 style={style.headline}>github-oneview</h3>
         <SelectRepo />
         <ContentList />
       </div>
